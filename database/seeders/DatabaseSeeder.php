f757861fb455a77faf9c6d84e2dc4db2908067c4
unknown <dedehnaaa@gmail.com>
2023-03-07 09:36:42 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'nama'=>'Administrator',
+            'username'=>'admin',
+            'password'=>bcrypt('password'),
+            'role'=>'admin',
+            'remember_token'=> Str::random(10),
+        ]);
+        User::create([
+            'nama'=>'Manajer',
+            'username'=>'manajer',
+            'password'=>bcrypt('password'),
+            'role'=>'manajer',
+            'remember_token'=> Str::random(10),
+        ]);
+        User::create([
+            'nama'=>'Kasir',
+            'username'=>'kasir',
+            'password'=>bcrypt('password'),
+            'role'=>'kasir',
+            'remember_token'=> Str::random(10),
+        ]);
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
